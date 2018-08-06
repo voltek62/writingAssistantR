0de59a46fd1bc32afaa6d8014c1909d821c926cf
vincent <vincent@yeswescore.com>
2018-08-06 22:37:02 +0200

    add test

--- tests/testthat/coconse_api.R
+++ tests/testthat/coconse_api.R
@@ -1,11 +1,16 @@
 
 context("cocon.se")
 
-test_that("The Key API is ok", {
-  expect_equal(initAPI(), "ok")
+test_that("The Token is ok", {
+  expect_equal(createToken(), "ok")
 })
 
 require(RCurl)
 require(rjson)
 require(XML)
+
+test_that("The status is ok", {
+  # must return ok
+  expect_equal(checkStatus(), "ok")
+})
 

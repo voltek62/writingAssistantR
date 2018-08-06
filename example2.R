625abd6adabfaed42f453861ed012db1e63303a3
vincent <vincent@yeswescore.com>
2018-08-06 21:31:51 +0200

    add new examples

---
+++ example2.R
@@ -0,0 +1,19 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+#   Generate Doc:              roxygen2::roxygenise()
+#   Update Package :           update.packages(ask=FALSE)
+
+library(RCurl)
+library(rjson)
+library(XML)
+
+createToken()
+
+checkStatus()
